6a900ae5e8dedf3e256a7cff1166f3e6d10e7a70
covajesus <jesuscova@jisparking.com>
2026-07-12 23:20:51 -0400

    cambios

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=9000, reload=True)
